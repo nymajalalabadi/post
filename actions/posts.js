d195cbf21b5e08d3fa34262f4173de241efc772b
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-12 08:44:22 -0600

    Added revalidatePath

--- actions/posts.js
+++ actions/posts.js
@@ -3,6 +3,7 @@ import { storePost } from '@/lib/posts';
 import { redirect } from 'next/navigation';
 import { uploadImage } from '@/lib/cloudinary';
 import { updatePostLikeStatus } from '@/lib/posts';
+import { revalidatePath } from 'next/cache';
 
 export async function createPost( prevState, formData) {
     const title = formData.get('title');
@@ -43,4 +44,5 @@ export async function createPost( prevState, formData) {
 
 export async function togglePostLikeStatus(postId) {
   updatePostLikeStatus(postId, 2);
+  revalidatePath('/', 'layout');
 }
