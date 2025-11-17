434bd5ae75fff7369da91bf4f4ec94130ea047d7
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-17 09:08:54 -0600

    Added Metadata

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,5 +1,20 @@
 import Posts from '@/component/posts';
 import { getPosts } from '@/lib/posts';
+import { Metadata } from 'next';
+
+// export const metadata: Metadata = {
+//   title: 'browse all our X posts',
+//   description: 'Browse all posts from all users',
+// };
+
+export const generateMetadata = async () => {
+  const posts = await getPosts();
+  const postCount = posts.length;
+  return {
+    title: `browse all our ${postCount} posts`,
+    description: `Browse all ${postCount} posts from all users`,
+  };
+};
 
 export default async function FeedPage() {
   const posts = await getPosts();
